3a487cb919d0d773024cfc05c6e161bf71e363d2
Sofiya <sk074866336@gmail.com>
2023-10-04 01:16:21 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Random rndm = new Random();
+int M = rndm.Next(1,11);
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i<M; i++)
+        array[i] = int.Parse(Console.ReadLine()!);
+        return array;
+}
+
+
+int SummPositive(int[] massive)
+{
+    int summ = 0;
+    for (int j = 0; j<M; j++)
+    {
+        if (massive[j]>0)
+            summ++;
+    }
+
+    return summ;
+}
+
+int[] numbers = new int[M];
+Console.WriteLine(FillArray(numbers));
+Console.WriteLine(SummPositive(numbers));
